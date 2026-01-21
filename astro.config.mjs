d3fd133a1f0c81025bedf7bb7abbd9c19a6124b9
Alexey Lapin <alexey-lapin@protonmail.com>
2026-01-20 22:40:21 -0500

    update favicon and inline assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,14 @@ import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
+    build: {
+        inlineStylesheets: 'always'
+    },
     vite: {
-        plugins: [yaml()]
+        plugins: [yaml()],
+        build: {
+            cssCodeSplit: false,
+            assetsInlineLimit: 100000000 // Inline all assets
+        }
     }
 });
